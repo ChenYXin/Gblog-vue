73ee498b6cbd69a12582cd193de59f0491e1d3e7
Donkor- <donkor@yeah.net>
2024-08-31 22:40:02 +0800

    首页列表，搜索功能

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -11,7 +11,30 @@
       </div>
       <div @click="dashboard">后台</div>
     </div>
-
+    <n-divider />
+    <n-space class="search">
+      <n-input
+        v-model:value="pageInfo.keyword"
+        :style="{ width: '500px' }"
+        placeholder="请输入关键字"
+      ></n-input>
+      <n-button type="primary" ghost @click="loadBlogs(0)">搜索</n-button>
+    </n-space>
+    <div v-for="(blog, index) in blogListInfo" style="margin-top: 15px">
+      <n-card :title="blog.title">
+        {{ blog.content }}
+        <template #footer>
+          <n-space align="center">
+            <div>发布时间：{{ blog.created_at }}</div>
+          </n-space>
+        </template>
+      </n-card>
+    </div>
+    <n-pagination
+      @update:page="loadBlogs"
+      v-model:page="pageInfo.pageSize"
+      :page-count="pageInfo.pageCount"
+    />
     <n-divider />
     <div class="footer">
       <div>Power by xxx</div>
@@ -32,9 +55,18 @@ const message = inject('message')
 const axios = inject('axios')
 
 const categoryOptions = ref([])
+const blogListInfo = ref([])
 const selectedCategory = ref(0)
+const pageInfo = reactive({
+  pageSize: 0,
+  pageNum: 2,
+  pageCount: 0,
+  count: 0,
+  keyword: ''
+})
 
 onMounted(() => {
+  loadBlogs()
   loadCategory()
 })
 
@@ -44,6 +76,30 @@ const categoryName = computed(() => {
   })
   return selectedOption ? selectedOption.label : ''
 })
+
+const loadBlogs = async (page) => {
+  if (page !== 0) {
+    pageInfo.pageSize = page - 1
+  } else {
+    pageInfo.pageSize = 0
+  }
+  let res = await axios.get(
+    `/api/v1/articleList?pageSize=${pageInfo.pageSize}&pageNum=${pageInfo.pageNum}&keyword=${pageInfo.keyword}`
+  )
+  let temp_rows = res.data.data
+  for (let row of temp_rows) {
+    if (row.content.length >= 100) {
+      row.content = row.content.slice(0, 100).toString() + '...'
+    }
+    let d = new Date(row.created_at)
+    row.created_at = `${d.getFullYear()}年${d.getMonth() + 1}月${d.getDate()}日`
+    // row.created_at = d.toLocaleString()
+  }
+  pageInfo.count = res.data.total
+  pageInfo.pageCount =
+    parseInt(pageInfo.count / pageInfo.pageNum) + (pageInfo.count % pageInfo.pageNum > 0 ? 1 : 0)
+  blogListInfo.value = temp_rows
+}
 
 const loadCategory = async () => {
   let res = await axios.get('/api/v1/categoryList')
@@ -65,6 +121,10 @@ const dashboard = () => {
 </script>
 
 <style lang="scss" scoped>
+.search {
+  margin-bottom: 15px;
+}
+
 .container {
   width: 100%;
   margin: 0 auto;
@@ -89,6 +149,7 @@ const dashboard = () => {
     }
   }
 }
+
 .footer {
   text-align: center;
   line-height: 25px;
