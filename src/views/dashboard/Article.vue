87f6d7efeed23d401dd74fbb0e38ca204db0a8a7
Donkor- <donkor@yeah.net>
2024-08-31 17:08:08 +0800

    文章列表分页功能

--- src/views/dashboard/Article.vue
+++ src/views/dashboard/Article.vue
@@ -1,7 +1,28 @@
 <template>
   <div>
-    <n-tabs default-value="add" justify-content="start" type="line">
-      <n-tab-pane name="oasis" tab="Oasis"> Wonderwall</n-tab-pane>
+    <n-tabs default-value="list" justify-content="start" type="line">
+      <n-tab-pane name="list" tab="文章列表">
+        <div v-for="(blog, index) in blogListInfo" style="margin-top: 15px">
+          <n-card :title="blog.title">
+            {{ blog.content }}
+            <template #footer>
+              <n-space align="center">
+                <div>发布时间：{{ blog.created_at }}</div>
+                <n-button>修改</n-button>
+                <n-button>删除</n-button>
+              </n-space>
+            </template>
+          </n-card>
+        </div>
+
+        <n-space>
+          <div @click="toPage(pageNum)" v-for="pageNum in pageInfo.pageCount">
+            <div :style="'color:' + (pageNum - 1 === pageInfo.pageSize ? 'blue' : 'green')">
+              {{ pageNum }}
+            </div>
+          </div>
+        </n-space>
+      </n-tab-pane>
       <n-tab-pane name="add" tab="添加文章">
         <n-form-item label="标题">
           <n-input v-model:value="addArticle.title" placeholder="请输入标题" />
@@ -42,12 +63,40 @@ const addArticle = reactive({
 })
 
 const categoryOptions = ref([])
-onMounted(() => {
-  loadCategory()
+const blogListInfo = ref([])
+
+const pageInfo = reactive({
+  pageSize: 0,
+  pageNum: 3,
+  pageCount: 0,
+  count: 0
 })
 
+onMounted(() => {
+  loadBlogs()
+  loadCategory()
+})
+const loadBlogs = async () => {
+  let res = await axios.get(
+    `/api/v1/articleList?pageSize=${pageInfo.pageSize}&pageNum=${pageInfo.pageNum}`
+  )
+  let temp_rows = res.data.data
+  for (let row of temp_rows) {
+    if (row.content.length >= 100) {
+      row.content = row.content.slice(0, 100).toString() + '...'
+    }
+    let d = new Date(row.created_at)
+    row.created_at = `${d.getFullYear()}年${d.getMonth() + 1}月${d.getDate()}日`
+    // row.created_at = d.toLocaleString()
+  }
+  pageInfo.count = res.data.total
+  pageInfo.pageCount =
+    parseInt(pageInfo.count / pageInfo.pageNum) + (pageInfo.count % pageInfo.pageNum > 0 ? 1 : 0)
+  blogListInfo.value = temp_rows
+}
+
 const loadCategory = async () => {
-  let res = await axios.get('/api/v1/categoryList', { headers: { token: adminStore.token } })
+  let res = await axios.get('/api/v1/categoryList')
   categoryOptions.value = res.data.data.map((item) => {
     return {
       label: item.name,
@@ -78,6 +127,11 @@ const add = async () => {
   } else {
     message.error(res.data.message)
   }
+}
+
+const toPage = async (pageNum) => {
+  pageInfo.pageSize = pageNum - 1
+  await loadBlogs()
 }
 </script>
 
